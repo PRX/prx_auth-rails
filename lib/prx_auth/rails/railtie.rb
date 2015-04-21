1cf4bf0b7424d49cf99911aed4f4d0d123959093
Chris Rhoden <carhoden@gmail.com>
2015-04-21 13:34:39 -0400

    remove logging

--- lib/prx_auth/rails/railtie.rb
+++ lib/prx_auth/rails/railtie.rb
@@ -1,5 +1,6 @@
 require 'rails/railtie'
 require 'prx_auth/rails/ext/controller'
+require 'rack/prx_auth'
 
 module PrxAuth
   module Rails
@@ -9,7 +10,6 @@ module PrxAuth
       end
 
       initializer 'prx_auth.insert_middleware' do |app|
-        p app.config.middleware
         app.config.middleware.insert_before ActionDispatch::ParamsParser, 'Rack::PrxAuth'
       end
     end
