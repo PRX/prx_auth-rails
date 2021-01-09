67c3a3449a68d929078f69dec478acb2a108987c
cavis <ryancavis@gmail.com>
2021-01-09 15:12:37 -0700

    Follow pattern

--- lib/prx_auth/rails/configuration.rb
+++ lib/prx_auth/rails/configuration.rb
@@ -5,7 +5,7 @@ class PrxAuth::Rails::Configuration
     @install_middleware = true
     if defined?(::Rails)
       klass = ::Rails.application.class
-      parent_name = if Rails::VERSION::MAJOR >= 6
+      parent_name = if ::Rails::VERSION::MAJOR >= 6
                       klass.module_parent_name
                     else
                       klass.parent_name
