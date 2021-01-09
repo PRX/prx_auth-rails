c06c31e07f129000aaecf88564752fb841e1f05c
cavis <ryancavis@gmail.com>
2021-01-09 15:10:30 -0700

    Support both parent_name and module_parent_name

--- lib/prx_auth/rails/configuration.rb
+++ lib/prx_auth/rails/configuration.rb
@@ -5,8 +5,13 @@ class PrxAuth::Rails::Configuration
     @install_middleware = true
     if defined?(::Rails)
       klass = ::Rails.application.class
-      klass_name = if klass.parent_name.present?
-                     klass.parent_name
+      parent_name = if Rails::VERSION::MAJOR >= 6
+                      klass.module_parent_name
+                    else
+                      klass.parent_name
+                    end
+      klass_name = if parent_name.present?
+                     parent_name
                    else
                      klass.name
                    end
