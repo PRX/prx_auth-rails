d164780882c44a0433c4a8a0078c5895a67711bb
Sam Vevang <sam.vevang@gmail.com>
2021-02-11 10:59:17 -0600

    Define application helper methods

--- lib/prx_auth/rails/engine.rb
+++ lib/prx_auth/rails/engine.rb
@@ -1,5 +1,9 @@
 module PrxAuth
   module Rails
-    class Engine < ::Rails::Engine; end
+    class Engine < ::Rails::Engine
+      config.to_prepare do
+        ::ApplicationController.helper_method [:current_user, :account_name_for]
+      end
+    end
   end
 end
