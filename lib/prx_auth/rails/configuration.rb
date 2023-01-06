0fb02269b60a09a1003a6d1579ae222257ee8b52
cavis <ryancavis@gmail.com>
2023-01-06 12:54:05 -0700

    Reformat and reconfigure test

--- lib/prx_auth/rails/configuration.rb
+++ lib/prx_auth/rails/configuration.rb
@@ -2,27 +2,34 @@ class PrxAuth::Rails::Configuration
   attr_accessor :install_middleware,
                 :namespace,
                 :prx_client_id,
-                :id_host
+                :id_host,
+                :cert_path
 
+  DEFAULT_ID_HOST = 'id.prx.org'
+  DEFAULT_CERT_PATH = 'api/v1/certs'
 
   def initialize
     @install_middleware = true
-    if defined?(::Rails)
-      klass = ::Rails.application.class
-      parent_name = if ::Rails::VERSION::MAJOR >= 6
-                      klass.module_parent_name
-                    else
-                      klass.parent_name
-                    end
-      klass_name = if parent_name.present?
-                     parent_name
-                   else
-                     klass.name
-                   end
+    @prx_client_id = nil
+    @id_host = DEFAULT_ID_HOST
+    @cert_path = DEFAULT_CERT_PATH
 
-      @namespace = klass_name.underscore.intern
-      @prx_client_id = nil
-      @id_host = nil
-    end
+    # infer default namespace from app name
+    @namespace =
+      if defined?(::Rails)
+        klass = ::Rails.application.class
+        parent_name = if ::Rails::VERSION::MAJOR >= 6
+                        klass.module_parent_name
+                      else
+                        klass.parent_name
+                      end
+        klass_name = if parent_name.present?
+                       parent_name
+                     else
+                       klass.name
+                     end
+
+        klass_name.underscore.intern
+      end
   end
 end
