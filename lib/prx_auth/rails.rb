f7873e7515db22c1d6e8aac676e20b747a5133cd
cavis <ryancavis@gmail.com>
2023-01-06 12:54:53 -0700

    Use configure block to trigger middleware install, with explicit id host

--- lib/prx_auth/rails.rb
+++ lib/prx_auth/rails.rb
@@ -6,10 +6,36 @@ require "prx_auth/rails/engine" if defined?(Rails)
 module PrxAuth
   module Rails
     class << self
-      attr_accessor :configuration
+      attr_accessor :configuration, :installed_middleware
 
       def configure
-        yield configuration
+        yield configuration if block_given?
+
+        # only install from first call to configure block
+        if configuration.install_middleware && !installed_middleware
+          install_middleware!
+          self.installed_middleware = true
+        end
+      end
+
+      def install_middleware!(app = nil)
+        app ||= ::Rails.application if defined?(::Rails)
+
+        return false unless app
+
+        # guess protocol from host
+        host = configuration.id_host
+        path = configuration.cert_path
+        protocol =
+          if host.include?('localhost') || host.include?('127.0.0.1')
+            'http'
+          else
+            'https'
+          end
+
+        app.middleware.insert_after Rack::Head, Rack::PrxAuth,
+          cert_location: "#{protocol}://#{host}/#{path}",
+          issuer: host
       end
     end
 
---
+++ test/prx_auth/rails_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+require 'pry'
+
+describe PrxAuth::Rails do
+
+  subject { PrxAuth::Rails }
+
+  it 'gets a configuration' do
+    assert_equal :test_app, subject.configuration.namespace
+    assert_equal '1234', subject.configuration.prx_client_id
+    assert_equal 'id.prx.test', subject.configuration.id_host
+    assert_equal 'api/v1/certs', subject.configuration.cert_path
+  end
+
+  it 'installs and configures prx_auth middleware' do
+    mw = MiniTest::Mock.new
+    mw.expect :insert_after, nil do |c1, c2, cert_location:, issuer:|
+      assert_equal Rack::Head, c1
+      assert_equal Rack::PrxAuth, c2
+      assert_equal 'https://id.prx.test/api/v1/certs', cert_location
+      assert_equal 'id.prx.test', issuer
+    end
+
+    app = MiniTest::Mock.new
+    app.expect :middleware, mw
+
+    subject.install_middleware!(app)
+    mw.verify
+  end
+
+  it 'installs middleware after configuration' do
+    called = false
+    spy = -> { called = true }
+
+    PrxAuth::Rails.stub(:install_middleware!, spy) do
+      PrxAuth::Rails.installed_middleware = false
+
+      PrxAuth::Rails.configure do |config|
+        config.install_middleware = true
+      end
+
+      assert PrxAuth::Rails.installed_middleware
+    end
+
+    assert called
+  end
+
+  it 'allows overriding of the middleware automatic installation' do
+    called = false
+    spy = -> { called = true }
+
+    PrxAuth::Rails.stub(:install_middleware!, spy) do
+      PrxAuth::Rails.installed_middleware = false
+
+      PrxAuth::Rails.configure do |config|
+        config.install_middleware = false
+      end
+
+      refute PrxAuth::Rails.installed_middleware
+    end
+
+    refute called
+  end
+end
