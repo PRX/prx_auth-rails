ed83896c5b5e36c7f6be957180d252025406e480
cavis <ryancavis@gmail.com>
2023-01-26 13:16:47 -0700

    New config

--- test/prx_auth/rails/configuration_test.rb
+++ test/prx_auth/rails/configuration_test.rb
@@ -7,6 +7,7 @@ describe PrxAuth::Rails::Configuration do
   it 'initializes with defaults' do
     assert subject.install_middleware
     assert_nil subject.prx_client_id
+    assert_nil subject.prx_scope
     assert_equal 'id.prx.org', subject.id_host
     assert_equal 'api/v1/certs', subject.cert_path
   end
@@ -20,6 +21,7 @@ describe PrxAuth::Rails::Configuration do
       PrxAuth::Rails.configure do |config|
         config.install_middleware = false
         config.prx_client_id = 'some-id'
+        config.prx_scope = 'appname:*'
         config.id_host = 'id.prx.blah'
         config.cert_path = 'cert/path'
         config.namespace = :new_test
@@ -28,6 +30,7 @@ describe PrxAuth::Rails::Configuration do
 
     refute subject.install_middleware
     assert_equal 'some-id', subject.prx_client_id
+    assert_equal 'appname:*', subject.prx_scope
     assert_equal 'id.prx.blah', subject.id_host
     assert_equal 'cert/path', subject.cert_path
     assert_equal :new_test, subject.namespace
