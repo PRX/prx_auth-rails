40995c468a9b1d4733280a602dff68a10dc1e264
cavis <ryancavis@gmail.com>
2025-03-19 13:30:52 -0600

    Fix tests - we check for read-private by default now

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate!
-
   def index
-  end
-
-  def after_sign_in_path_for(_resource)
-    "/after-sign-in-path"
   end
 end
